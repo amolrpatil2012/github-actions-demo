30e74087620ac2ceb9849de64ab7f7b1f38c379e
AMOL PATIL <136699553+amolrpatil2012@users.noreply.github.com>
2026-09-10 20:36:25 +0530

    Update HelloController.java

--- src/main/java/com/example/github_actions/HelloController.java
+++ src/main/java/com/example/github_actions/HelloController.java
@@ -10,5 +10,9 @@ public class HelloController {
     public String hello() {
         return "Hello, World!";
     }
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome";
+    }
 
 }
